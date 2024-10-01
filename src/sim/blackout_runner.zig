f58a68a9d9430663f4e3646748d7c9bca2c61621
Andrew Hessler <adhessler@gmail.com>
2024-09-30 21:11:11 -0500

    sim working!

--- src/sim/blackout_runner.zig
+++ src/sim/blackout_runner.zig
@@ -16,46 +16,98 @@ pub const BlackoutRunner = struct {
         for (card.n_column, 0..) |value, i| {
             fixed_n_columns[i] = value orelse 0;
         }
+
         var columns: [5][5]u8 = .{ card.b_column, card.i_column, fixed_n_columns, card.g_column, card.o_column };
-        var values: [25]u8 = undefined;
         for (&columns) |*column| {
-            std.mem.sort(u8, column, {}, comptime std.sort.asc(u8));
-            values = mergeSortedArraysAsc(&values, column[0..]);
+            std.mem.sort(u8, column, {}, std.sort.asc(u8));
         }
-        std.debug.print("{any}", .{card.b_column});
-        std.debug.print("{any}", .{self.num});
+
+        var games_won_by_turn: [75]u64 = std.mem.zeroes([75]u64);
+        for (0..1_000_000_000) |_| {
+            const call_order = getRandomCallOrder(self.prng);
+            const turn_won = runSim(call_order, columns);
+            games_won_by_turn[turn_won] += 1;
+        }
+
+        var total_games_52_or_less: u64 = 0;
+        for (games_won_by_turn, 0..) |games_won, i| {
+            std.debug.print("Games won turn {d}: {d}\n", .{ i + 1, games_won });
+            if (i < 52) {
+                total_games_52_or_less += games_won;
+            }
+        }
+        const total_games_less_than: f64 = @floatFromInt(total_games_52_or_less);
+        std.debug.print("Games <=52: {d}\n", .{total_games_52_or_less});
+        std.debug.print("Percent Win <=52: {d}\n", .{total_games_less_than / 1_000_000_000.0});
+    }
+
+    fn runSim(call_order: [75]u8, columns: [5][5]u8) u8 {
+        var values_found: u8 = 0;
+        var turn_won: u8 = 0;
+        for (call_order, 0..) |value, i| {
+            const column: u8 = @divFloor(value - 1, 15);
+            for (columns[column]) |column_value| {
+                if (column_value == value) {
+                    values_found += 1;
+                }
+            }
+            switch (value) {
+                1...15 => {
+                    for (columns[0]) |b_value| {
+                        if (b_value == value) {
+                            values_found += 1;
+                        }
+                    }
+                },
+                16...30 => {
+                    for (columns[1]) |i_value| {
+                        if (i_value == value) {
+                            values_found += 1;
+                        }
+                    }
+                },
+                31...45 => {
+                    for (columns[2]) |n_value| {
+                        if (n_value == value) {
+                            values_found += 1;
+                        }
+                    }
+                },
+                46...60 => {
+                    for (columns[3]) |g_value| {
+                        if (g_value == value) {
+                            values_found += 1;
+                        }
+                    }
+                },
+                61...75 => {
+                    for (columns[4]) |o_value| {
+                        if (o_value == value) {
+                            values_found += 1;
+                        }
+                    }
+                },
+                else => unreachable,
+            }
+            if (values_found == 24) {
+                turn_won = @intCast(i);
+                break;
+            }
+        }
+        return turn_won;
     }
 };
 
-fn mergeSortedArraysAsc(array_one: *[25]u8, array_two: *[5]u8) []u8 {
-    var count_one = 0;
-    var count_two = 0;
-    var merged_array: [array_one.len + array_two.len]u8 = undefined;
-    var i = 0;
-
-    while (count_one < array_one.len and count_two < array_two.len) {
-        if (array_one[count_one] < array_two[count_two]) {
-            merged_array[i] = array_one[count_one];
-            count_one += 1;
-        } else {
-            merged_array[i] = array_two[count_two];
-            count_two += 1;
-        }
-        i += 1;
+fn getRandomCallOrder(prng: *std.Random.DefaultPrng) [75]u8 {
+    var bingo_balls: [75]u8 = undefined;
+    inline for (&bingo_balls, 0..) |*ball, i| {
+        ball.* = @intCast(i + 1);
     }
-    if (count_one == array_one.len) {
-        merged_array = merged_array ++ array_two[count_two..array_two.len];
-    } else {
-        merged_array = merged_array ++ array_one[count_one..array_one.len];
+    for (0..75) |i| {
+        const j = prng.random().intRangeAtMost(u8, @intCast(i), @intCast(bingo_balls.len - 1));
+        const temp = bingo_balls[i];
+        bingo_balls[i] = bingo_balls[j];
+        bingo_balls[j] = temp;
     }
-
-    return merged_array;
-}
-
-test "merges sorted arrays" {
-    const array_one = .{ 1, 2, 3, 10 };
-    const array_two = .{ 5, 6, 8, 14, 17 };
-
-    const merged_array = mergeSortedArraysAsc(array_one, array_two);
-    std.testing.expect(std.mem.eql(merged_array, .{ 1, 2, 3, 5, 6, 8, 10, 14, 17 }));
+    return bingo_balls;
 }
